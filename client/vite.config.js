01c17e3d8058b105494706eee119849f53ea6e27
Ujjawal Kumar <ujjawalrr@gmail.com>
2024-04-13 11:49:32 +0530

    proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: import.meta.env.SERVER_URL,
         secure: false,
       },
     },
